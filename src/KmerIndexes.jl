76ade844b3aff380409a33a221313df21c671548
Kenta Sato <bicycle1885@gmail.com>
2015-11-21 20:20:36 +0900

    add KmerIndex

--- src/KmerIndexes.jl
+++ src/KmerIndexes.jl
@@ -1,5 +1,35 @@
 module KmerIndexes
 
-# package code goes here
+export KmerIndex, count, locate
+
+using Bio.Seq
+
+"""
+Dense k-mer index.
+"""
+immutable KmerIndex{K,T}
+    data::Vector{Vector{T}}
+end
+
+function KmerIndex{K}(::Type{DNAKmer{K}}, seq::DNASequence, step::Integer=1)
+    data = Vector{Vector{Int}}(4^K)
+    for idx in 1:endof(data)
+        data[idx] = Vector{Int}()
+    end
+    for (pos, kmer) in each(DNAKmer{K}, seq, step)
+        idx = UInt64(kmer)
+        push!(data[idx], pos)
+    end
+    return KmerIndex{K,Int}(data)
+end
+
+function Base.count{K}(kmer::DNAKmer{K}, index::KmerIndex{K})
+    return length(locate(kmer, index))
+end
+
+function locate{K}(kmer::DNAKmer{K}, index::KmerIndex{K})
+    idx = UInt64(kmer)
+    return index.data[idx]
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using KmerIndexes
-using Base.Test
+using BaseTestNext
+using Bio.Seq
 
-# write your own tests here
-@test 1 == 1
+@testset "short sequence" begin
+    seq = dna"ACGTAGACGT"
+    index = KmerIndex(DNAKmer{4}, seq)
+    @test count(kmer(dna"ACGT"), index) === 2
+    @test locate(kmer(dna"ACGT"), index) == [1, 7]
+    @test count(kmer(dna"AGAC"), index) === 1
+    @test locate(kmer(dna"AGAC"), index) == [5]
+end
